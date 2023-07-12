35cbda13e7837b63dfc023a6f2d3fcaa285747f8
Marcelo Alarcón <marceloalarconbarrenechea@gmail.com>
2023-07-12 17:04:31 -0400

    Update game id

--- src/modules/render.js
+++ src/modules/render.js
@@ -1,50 +1,45 @@
-import Player from './players.js';
-import ringAnimation from './animation.js';
-import src from '../assets/images/user.png';
+import Player from "./players.js";
+import ringAnimation from "./animation.js";
+import src from "../assets/images/user.png";
 
 const UpdateScores = (player) => {
-  player.getPlayers()
-    .then((response) => {
-      const leaderBoard = response.result.sort((a, b) => b.score - a.score);
-      let aux = '';
-      leaderBoard.forEach((elem) => {
-        aux += `<li class="player-scores flex">
+  player.getPlayers().then((response) => {
+    const leaderBoard = response.result.sort((a, b) => b.score - a.score);
+    let aux = "";
+    leaderBoard.forEach((elem) => {
+      aux += `<li class="player-scores flex">
         <img src="${src}" class="user-img" />
         <p class="p-user">${elem.user} : ${elem.score}</p>
         </li>`;
-      });
-      document.querySelector('ul.player-scores').innerHTML = aux;
-      ringAnimation();
     });
+    document.querySelector("ul.player-scores").innerHTML = aux;
+    ringAnimation();
+  });
 };
 
 const renderScore = () => {
-  const player = new Player(
-    'N6LN8OdSQs7L7qxDSROl',
-    'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/',
-  );
+  const player = new Player("fadHZHQsGimScUUX6oii", "https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/");
   // fill the scores table
   UpdateScores(player);
 
   /* -------------------------------------------------------------------------- */
   /*                               Event listener                               */
   /* -------------------------------------------------------------------------- */
-  const form = document.querySelector('#scoreForm');
-  form.addEventListener('submit', (event) => {
+  const form = document.querySelector("#scoreForm");
+  form.addEventListener("submit", (event) => {
     event.preventDefault();
     const objPlayer = {
       user: form.elements.name.value,
       score: form.elements.score.value,
     };
-    player.addPlayer(objPlayer)
-      .then((resolve) => {
-        document.querySelector('#msg').textContent = resolve;
-        form.elements.name.value = '';
-        form.elements.score.value = '';
-      });
+    player.addPlayer(objPlayer).then((resolve) => {
+      document.querySelector("#msg").textContent = resolve;
+      form.elements.name.value = "";
+      form.elements.score.value = "";
+    });
   });
 
-  document.querySelector('#btnRefresh').addEventListener('click', () => {
+  document.querySelector("#btnRefresh").addEventListener("click", () => {
     UpdateScores(player);
   });
 };
